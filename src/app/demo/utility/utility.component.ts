41aa8a10c2e09db90b716886098906504216dd9c
Tushar <tusharnimje21@gmail.com>
2020-06-03 18:25:32 +0530

    updated utility component

--- src/app/demo/utility/utility.component.ts
+++ src/app/demo/utility/utility.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import Detect from 'tutility/detect';
 
 @Component({
   selector: 'app-utility',
   templateUrl: './utility.component.html',
   styleUrls: ['./utility.component.scss']
 })
-export class UtilityComponent { }
+export class UtilityComponent {
+
+  deviceDetail: any;
+
+  detecBrowserHandler(): void {
+    const detect = new Detect();
+    this.deviceDetail = detect.parse(navigator.userAgent);
+    console.log(this.deviceDetail);
+  }
+}
